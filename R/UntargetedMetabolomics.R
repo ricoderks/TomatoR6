f959d2a4205beaf9bd078284ed5e43eec42e1cc0
Rico Derks <r.j.e.derks@lumc.nl>
2025-02-19 09:05:52 +0100

    Preparing untargeted metabolomics class definition.

--- R/UntargetedMetabolomics.R
+++ R/UntargetedMetabolomics.R
@@ -13,26 +13,28 @@ NULL
 UntargetedMetabolomics <- R6::R6Class(
   inherit = DataImport,
   classname = "UntargetedMetabolomics",
-  #----------------------------------------------------------------- public ----
+  #----------------------------------------------------------------- PUBLIC ----
   public = list(
     initialize = function(name = NA) {
       super$initialize(name)
     }
-    
-    
-    #---------------------------------------------------------- global info ----    
-    
-    
-    #------------------------------------------------------- import methods ----
-    
-    
-    #------------------------------------------ convert to standard methods ----
-    # Rico: Methods to reshape / restructure the data to the standardized format
-    # we specified.
-    
-    
-    #----------------------------------------------------- plotting methods ----
-    
-    
-  )
+  ), # end public
+  #---------------------------------------------------------------- PRIVATE ----
+  private = list(
+    import_data = function() {
+      import_read_msdial(self = self)
+      private$extract_featuredata()
+      private$make_data_long()
+      private$make_data_wide()
+    },
+    make_data_long = function() {
+      make_table_long(self = self)
+    },
+    make_data_wide = function() {
+      make_table_wide(self = self)
+    },
+    extract_featuredata = function() {
+      extract_metabolite_data(self = self)
+    }
+  ) # end private
 )
